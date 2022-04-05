2cc32488306db1c1d1947f13e1994933cfa4dc81
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-04-05 16:06:16 +0900

    feat: get todos, get todo by id & 에러처리

--- back/server.js
+++ back/server.js
@@ -17,43 +17,101 @@ const PORT = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
-server.post("/todo/create", async (req, res) => {
-  await db.read();
-  const { id: lastId } = db.data.lastTodoId;
-
-  const {
-    body: { title, desc, author, column },
-  } = req;
-
-  const createdAt = getDate();
-  const updatedAt = createdAt;
-
-  const newTodo = db.data.todo.push({
-    id: lastId + 1,
-    title,
-    desc,
-    author,
-    column,
-    createdAt,
-    updatedAt,
-  });
-
-  if (!newTodo) {
-    throw Error("push 에러");
+/**
+ * todo controllers
+ */
+const getTodos = async () => {
+  try {
+    await db.read();
+    const todos = db.data.todo;
+    if (!todos) {
+      throw Error("Todo 리스트를 찾지 못했습니다.");
+    }
+    return {
+      ok: true,
+      results: todos,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      message: error.message,
+    };
   }
+};
 
-  db.data.lastTodoId = { id: lastId + 1 };
-  await db.write();
-  res.send({
-    ok: true,
-  });
+const getTodoById = async (id) => {
+  try {
+    const { results: todos } = await getTodos();
+    const todo = todos.find((todo) => todo.id === +id);
+    if (!todo) {
+      throw Error("Todo 를 찾지 못했습니다.");
+    }
+    return {
+      ok: true,
+      results: todo,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      message: error.message,
+    };
+  }
+};
+
+const postTodoCreate = async ({ title, desc, author, column }) => {
+  try {
+    await db.read();
+
+    const { id: lastId } = db.data.lastTodoId;
+    const createdAt = getDate();
+    const updatedAt = createdAt;
+
+    const newTodo = {
+      id: lastId + 1,
+      title,
+      desc,
+      author,
+      column,
+      createdAt,
+      updatedAt,
+    };
+
+    db.data.todo.push(newTodo);
+
+    db.data.lastTodoId = { id: lastId + 1 };
+    await db.write();
+
+    return {
+      ok: true,
+      results: newTodo,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      message: error.message,
+    };
+  }
+};
+/**
+ * todo routes
+ */
+server.post("/todo/create", async (req, res) => {
+  const { body } = req;
+  const sendData = await postTodoCreate(body);
+  res.send(sendData);
 });
 
-server.get("/todo/:id", (req, res) => {
+server.get("/todo", async (req, res) => {
+  const sendData = await getTodos();
+  res.send(sendData);
+});
+
+server.get("/todo/:id", async (req, res) => {
   const {
     params: { id },
   } = req;
-  res.send(db.get("todo").find({ id }).value());
+  const sendData = await getTodoById(id);
+  res.send(sendData);
 });
 
 server.use(router);
