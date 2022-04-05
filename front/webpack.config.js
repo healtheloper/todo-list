f4e6471236ac831cefc13f30265d50d88812e011
BB-choi <choibobae0809@gmail.com>
2022-04-05 16:11:37 +0900

    env: 플러그인 추가
    - cleanWebPackPlugin
    - ErrorOverlayPlugin
    - ESLintPlugin

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ErrorOverlayPlugin = require("error-overlay-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
@@ -11,6 +13,9 @@ module.exports = {
     filename: "bundle.js",
   },
   plugins: [
+    new ErrorOverlayPlugin(),
+    new ESLintPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
