6f530dcc6a97bbeab121f3527cb73463a055d3ea
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-04-11 13:14:22 +0900

    feat: swagger 연결, /api-docs

--- back/src/app.js
+++ back/src/app.js
@@ -1,14 +1,17 @@
 import bodyParser from "body-parser";
 import express from "express";
+import swaggerJSdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
-import columnRouter from "./routers/columnRouter";
-import logRouter from "./routers/logRouter";
-import todoRouter from "./routers/todoRouter";
+import router from "./router";
+import swaggerOptions from "./swagger/options";
 
 const app = express();
+const swaggerSpecs = swaggerJSdoc(swaggerOptions);
+
 app.use(bodyParser.json());
-app.use("/todo", todoRouter);
-app.use("/column", columnRouter);
-app.use("/log", logRouter);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+app.use("/api", router);
 
 export default app;
--- back/src/routers/todoRouter.js
+++ back/src/routers/todoRouter.js
@@ -10,10 +10,86 @@ import {
 
 const todoRouter = express.Router();
 
+/**
+ * @swagger
+ *  /todo:
+ *    get:
+ *      summary: "Todo 리스트 검색"
+ *      tags: [Todo]
+ *      responses:
+ *        "200":
+ *          description: A Todo schema
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Todo'
+ */
 todoRouter.get("/", getTodos);
+
+/**
+ * @swagger
+ *  /todo/:id:
+ *    get:
+ *      summary: 특정 Todo 검색
+ *      tags: [Todo]
+ *      responses:
+ *        "200":
+ *          description: A Todo schema
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Todo'
+ */
 todoRouter.get("/:id", getTodoById);
+
+/**
+ * @swagger
+ *  /todo/create:
+ *    post:
+ *      summary: Todo 생성
+ *      tags: [Todo]
+ *      responses:
+ *        "200":
+ *          description: A Todo schema
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Todo'
+ */
 todoRouter.post("/create", postTodoCreate);
+
+/**
+ * @swagger
+ * path:
+ *  /todo/delete/:id:
+ *    delete:
+ *      summary: Todo 삭제
+ *      tags: [Todo]
+ *      responses:
+ *        "200":
+ *          description: A Todo schema
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Todo'
+ */
 todoRouter.delete("/delete/:id", deleteTodoById);
+
+/**
+ * @swagger
+ * path:
+ *  /todo/update/:id:
+ *    patch:
+ *      summary: Todo 수정
+ *      tags: [Todo]
+ *      responses:
+ *        "200":
+ *          description: A Todo schema
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Todo'
+ */
 todoRouter.patch("/update/:id", updateTodoById);
 
 export default todoRouter;
