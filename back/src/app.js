77f293500aadb6b8371f4a393a0e9427f1bc582e
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-04-11 13:31:22 +0900

    refactor: app 내부 swagger 관련 로직 분리

--- back/src/app.js
+++ back/src/app.js
@@ -1,17 +1,14 @@
 import bodyParser from "body-parser";
 import express from "express";
-import swaggerJSdoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
 
 import router from "./router";
-import swaggerOptions from "./swagger/options";
+import swagger from "./swagger/swagger";
 
 const app = express();
-const swaggerSpecs = swaggerJSdoc(swaggerOptions);
 
 app.use(bodyParser.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+app.use("/api-docs", swagger);
 app.use("/api", router);
 
 export default app;
