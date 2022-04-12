ef859c2d0357d68a7d4aaca020c584f07bac9a6a
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-04-12 23:44:19 +0900

    feat: get column by id

--- back/src/routers/columnRouter.js
+++ back/src/routers/columnRouter.js
@@ -1,6 +1,10 @@
 import express from "express";
 
-import { getColumns, postColumnCreate } from "../controllers/columnController";
+import {
+  getColumns,
+  getColumnById,
+  postColumnCreate,
+} from "../controllers/columnController";
 
 const columnRouter = express.Router();
 
@@ -26,6 +30,32 @@ const columnRouter = express.Router();
  *                      $ref: '#/components/schemas/Column'
  */
 columnRouter.get("/", getColumns);
+
+/**
+ * @swagger
+ *  /column/{id}:
+ *    get:
+ *      summary: "특정 Column 검색"
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *      tags: [Column]
+ *      responses:
+ *        "200":
+ *          description: id 를 통해 특정 Column 겁색
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  ok:
+ *                    type: boolean
+ *                  results:
+ *                    type: object
+ *                    $ref: '#/components/schemas/Column'
+ */
+columnRouter.get("/:id", getColumnById);
 
 /**
  * @swagger
