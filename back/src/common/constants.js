2a2d33a3743adedadbaf3608172609095dd259f9
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-04-13 21:58:01 +0900

    fix: url 잘못 설정한 것 수정

--- back/src/common/constants.js
+++ back/src/common/constants.js
@@ -17,6 +17,6 @@ const BASE_URL = {
   development: "http://localhost:",
 };
 
-export const SERVICE_DOMAIN = "https://healtheloper.github.io/todo-list/";
+export const SERVICE_DOMAIN = "https://healtheloper.github.io";
 
 export const API_URL = (PORT) => `${BASE_URL[process.env.NODE_ENV]}${PORT}/`;
