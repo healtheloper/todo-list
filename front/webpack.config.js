e2e8f0205457cbe2386039c058c0ae66549c63f1
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-04-13 16:20:07 +0900

    chore: DEPLOY 환경변수 설정

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -4,34 +4,41 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const ErrorOverlayPlugin = require("error-overlay-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { webpack } = require("webpack");
 
-module.exports = {
-  mode: "development",
-  entry: ["./src/index.js"],
-  devtool: "source-map",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  plugins: [
-    new ErrorOverlayPlugin(),
-    new ESLintPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
+module.exports = (env) => {
+  const { DEPLOY } = env;
+  return {
+    mode: "development",
+    entry: ["./src/index.js"],
+    devtool: "source-map",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+    plugins: [
+      new ErrorOverlayPlugin(),
+      new ESLintPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+      new webpack.DefinePlugin({
+        DEPLOY,
+      }),
     ],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+      ],
+    },
+  };
 };
