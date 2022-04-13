302cbd7b574d9f2a12f59b9e47795c31c60899e5
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-04-13 21:48:05 +0900

    feat: cors access url update

--- back/src/middlewares/cors.js
+++ back/src/middlewares/cors.js
@@ -1,6 +1,21 @@
+import { API_URL } from "../common/constants";
+
 const cors = (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
+  const isProduction = process.env.NODE_ENV === "production";
+  const accessControls = [
+    {
+      key: "Access-Control-Allow-Origin",
+      value: isProduction ? API_URL(process.env.PORT) : "*",
+    },
+    {
+      key: "Access-Control-Allow-Methods",
+      value: isProduction ? API_URL(process.env.PORT) : "*",
+    },
+  ];
+  accessControls.forEach((control) => {
+    const { key, value } = control;
+    res.setHeader(key, value);
+  });
   next();
 };
 
