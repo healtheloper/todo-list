ec3e74c6ddc488a5d5ea4ebaa92c9ae652b28d68
BB-choi <choibobae0809@gmail.com>
2022-04-13 22:24:39 +0900

    feat: column 제목 영역  +버튼,x버튼 클릭 이벤트

--- front/src/components/Content/CardWritable/CardWritable.js
+++ front/src/components/Content/CardWritable/CardWritable.js
@@ -1,34 +1,55 @@
+import peact from "../../../core/peact";
 import styles from "./cardWritable.module.css";
 
 const CardWritable = () => {
-  return `
-    <form class="${styles.cardWritable}">
-      <div class="${styles.headerArea}">
-        <input type="text" value="" class="${styles.cardTitleInput}" name="card-title" placeholder="제목을 입력하세요"/>
-        <div class="button">
-          <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.5 11.25L0.75 10.5L5.25 6L0.75 1.5L1.5 0.75L6 5.25L10.5 0.75L11.25 1.5L6.75 6L11.25 10.5L10.5 11.25L6 6.75L1.5 11.25Z"
-              fill="#828282"
-            />
-          </svg>
-        </div>
-      </div>
-      <div>
-        <input type="text" value="" class="${styles.cardDescInput}" name="card-content" placeholder="내용을 입력하세요"/>
-      </div>
-      <div class="${styles.buttons}">
-        <button type="button" class="${styles.button} ${styles.cancelButton}">취소</button>
-        <button type="submit" class="${styles.button} ${styles.confirmButton} ${styles.activeButton}">등록</button>
-      </div>
-    </form>
-  `;
+  const $inputDesc = peact.createElement({
+    tag: "div",
+    child: [
+      `<input type="text" value="" class="${styles.cardDescInput}" name="card-content" placeholder="내용을 입력하세요"/>`,
+    ],
+  });
+
+  const $inputTitle = peact.createElement({
+    tag: "input",
+    className: styles.cardTitleInput,
+    attrs: {
+      value: "",
+      type: "text",
+      name: "name-title",
+      placeholder: "제목을 입력하세요",
+    },
+    child: [],
+  });
+
+  const $cardWritableHeader = peact.createElement({
+    tag: "div",
+    className: styles.headerArea,
+    child: [$inputTitle],
+  });
+
+  const $cancelButton = peact.createElement({
+    tag: "button",
+    className: [styles.button, styles.cancelButton],
+    child: ["취소"],
+  });
+
+  const $confirmButton = peact.createElement({
+    tag: "button",
+    className: [styles.button, styles.confirmButton, styles.activeButton],
+    child: ["등록"],
+  });
+
+  const $buttonArea = peact.createElement({
+    tag: "div",
+    className: styles.buttons,
+    child: [$cancelButton, $confirmButton],
+  });
+
+  return peact.createElement({
+    tag: "div",
+    className: [styles.cardWritable, "cardWritable", styles.hidden],
+    child: [$cardWritableHeader, $inputDesc, $buttonArea],
+  });
 };
 
 export default CardWritable;
--- front/src/components/Content/ColumnHeader/ColumnHeader.js
+++ front/src/components/Content/ColumnHeader/ColumnHeader.js
@@ -1,35 +1,117 @@
 import peact from "../../../core/peact";
+import CardWritable from "../CardWritable/CardWritable";
 import styles from "./columnHeader.module.css";
 
 const ColumnHeader = ({ column, todos }) => {
+  const handleButton = ({ target, targetClassName, toggleClassName }) => {
+    const targetButton =
+      target.closest(`.${styles[targetClassName]}`) || target;
+    targetButton.classList.toggle(styles[toggleClassName]);
+  };
+
+  const onPlusButtonOver = ({ target }) => {
+    handleButton({
+      target,
+      targetClassName: "plusButton",
+      toggleClassName: "plusButtonOver",
+    });
+  };
+
+  const onPlusButtonOut = ({ target }) => {
+    handleButton({
+      target,
+      targetClassName: "plusButton",
+      toggleClassName: "plusButtonOver",
+    });
+  };
+
+  const onPlusButtonClick = ({ target }) => {
+    const $currentHeader = target.closest(".header");
+    const $cardWritable = $currentHeader.querySelector(".cardWritable");
+    $cardWritable.classList.toggle(styles.display);
+  };
+
+  const onXButtonOver = ({ target }) => {
+    handleButton({
+      target,
+      targetClassName: "xButton",
+      toggleClassName: "xButtonOver",
+    });
+  };
+
+  const onXButtonOut = ({ target }) => {
+    handleButton({
+      target,
+      targetClassName: "xButton",
+      toggleClassName: "xButtonOver",
+    });
+  };
+
+  const onXButtonClick = ({ target }) => {
+    const $currentHeader = target.closest(".header");
+    const $cardWritable = $currentHeader.querySelector(".cardWritable");
+    $cardWritable.classList.remove(styles.display);
+  };
+
   const todosCount = todos.length;
 
-  const headerInnerHTML = `
-        <div class="${styles.titleWrap}">
-            <h2 class="${styles.title}">${column.title}</h2>
-            <div class="${styles.count}">${todosCount}</div>
-        </div>
-        <div class="${styles.buttonArea}">
-            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0.105713 7.53033L0.105713 6.46967H6.46967V0.105713H7.53033V6.46967H13.8943V7.53033H7.53033V13.8943H6.46967V7.53033H0.105713Z" fill="#BDBDBD"/>
-            </svg>
-            
-            <path
-                d="M0.105709 7.53033L0.105709 6.46967H6.46967V0.105713H7.53033V6.46967H13.8943V7.53033H7.53033V13.8943H6.46967V7.53033H0.105709Z"
-                fill="black"
-            />
-            </svg>
+  const $columnTitleWrap = peact.createElement({
+    tag: "div",
+    className: styles.titleWrap,
+    child: [
+      `
+      <h2 class="${styles.title}">${column.title}</h2>
+      <div class="${styles.count}">${todosCount}</div>
+      `,
+    ],
+  });
 
-            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.5 11.25L0.75 10.5L5.25 6L0.75 1.5L1.5 0.75L6 5.25L10.5 0.75L11.25 1.5L6.75 6L11.25 10.5L10.5 11.25L6 6.75L1.5 11.25Z" fill="#BDBDBD"/>
-            </svg>
-            
-        </div>
-  `;
+  const $plusButton = peact.createElement({
+    tag: "div",
+    className: styles.plusButton,
+    attrs: {
+      onMouseOver: onPlusButtonOver,
+      onMouseOut: onPlusButtonOut,
+      onClick: onPlusButtonClick,
+    },
+    child: [
+      `<svg width="14" height="14" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.105713 7.53033L0.105713 6.46967H6.46967V0.105713H7.53033V6.46967H13.8943V7.53033H7.53033V13.8943H6.46967V7.53033H0.105713Z"/>
+      </svg>`,
+    ],
+  });
+
+  const $deleteButton = peact.createElement({
+    tag: "div",
+    className: styles.xButton,
+    attrs: {
+      onMouseOver: onXButtonOver,
+      onMouseOut: onXButtonOut,
+      onClick: onXButtonClick,
+    },
+    child: [
+      `<svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.5 11.25L0.75 10.5L5.25 6L0.75 1.5L1.5 0.75L6 5.25L10.5 0.75L11.25 1.5L6.75 6L11.25 10.5L10.5 11.25L6 6.75L1.5 11.25Z"/>
+      </svg>`,
+    ],
+  });
+
+  const $columnButtonArea = peact.createElement({
+    tag: "div",
+    className: styles.buttonArea,
+    child: [$plusButton, $deleteButton],
+  });
+
+  const $columnTitleArea = peact.createElement({
+    tag: "div",
+    className: styles.titleArea,
+    child: [$columnTitleWrap, $columnButtonArea],
+  });
+
   return peact.createElement({
     tag: "div",
-    className: styles.header,
-    child: [headerInnerHTML],
+    className: [styles.header, "header"],
+    child: [$columnTitleArea, CardWritable()],
   });
 };
 
