1673540c62d7bda317f78c0a34b51083ea8f872e
BB-choi <choibobae0809@gmail.com>
2022-04-13 15:32:04 +0900

    refactor: getAttribute("id")를 id로 변경
    - cardInnerHTML에서 xButton부분을 가독성을 위하여 분리

--- front/src/components/Content/Card/Card.js
+++ front/src/components/Content/Card/Card.js
@@ -32,28 +32,32 @@ const Card = ({ todo, handleRenderFlag }) => {
     if (!target.classList.contains(styles.xButton)) {
       return;
     }
-    const todoId = target.closest(`.${styles.card}`).getAttribute("id");
+    const todoId = target.closest(`.${styles.card}`).id;
     showAlert({ todoId });
   };
+
+  const xButton = `
+    <svg
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="${styles.xButton}"
+  >
+      <path
+        d="M1.5 11.25L0.75 10.5L5.25 6L0.75 1.5L1.5 0.75L6 5.25L10.5 0.75L11.25 1.5L6.75 6L11.25 10.5L10.5 11.25L6 6.75L1.5 11.25Z"
+        class="${styles.path}"
+      />
+    </svg>
+  `;
 
   const cardInnerHTML = `
     <div id="${todo._id}" name="${todo.columnId}" class="${styles.card}">
       <div class="${styles.headerArea}">
         <label class="${styles.title}">${todo.title}</label>
         <div>
-          <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            class="${styles.xButton}"
-          >
-            <path
-              d="M1.5 11.25L0.75 10.5L5.25 6L0.75 1.5L1.5 0.75L6 5.25L10.5 0.75L11.25 1.5L6.75 6L11.25 10.5L10.5 11.25L6 6.75L1.5 11.25Z"
-              class="${styles.path}"
-            />
-          </svg>
+          ${xButton}
         </div>
       </div>
       <div>
