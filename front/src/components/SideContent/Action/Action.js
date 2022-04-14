0d4a228c1de521c4d3c059026662a06791c4ef24
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-04-14 11:18:13 +0900

    refactor: LOG_TYPE 대문자 변경에 따른 uppercase

--- front/src/components/SideContent/Action/Action.js
+++ front/src/components/SideContent/Action/Action.js
@@ -16,7 +16,7 @@ const Action = ({ todoLog }) => {
         <p class="author">${author}</p>
         <p class="content">
             <strong>${columnTitle}</strong>에 <strong>${title}</strong>를
-            <strong>${LOG_TYPE[type]}</strong>하였습니다.
+            <strong>${LOG_TYPE[type.toUpperCase()]}</strong>하였습니다.
         </p>
         <p class="${styles.time}">1분 전</p>
     </div>
